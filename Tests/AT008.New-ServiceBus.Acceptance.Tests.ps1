3cd5fc08eea3c1adac5decff054e5b81891ddb1f
Craig Gumbley <craiggumbley@gmail.com>
2017-07-20 21:27:26 +0100

    serviceBusQueueName is now an array in config + formatting

--- Tests/AT008.New-ServiceBus.Acceptance.Tests.ps1
+++ Tests/AT008.New-ServiceBus.Acceptance.Tests.ps1
@@ -4,26 +4,26 @@ Push-Location -Path $PSScriptRoot\..\Infrastructure\Resources\
 Describe "New-ServiceBus Tests" -Tag "Acceptance-ARM" {
 
     It "Should create a Service Bus and return one output" {
-        $params = @{
+        $Params = @{
             Location = $Config.location
-            ResourceGroupName =  $Config.resourceGroupName
+            ResourceGroupName = $Config.resourceGroupName
             NamespaceName = $Config.serviceBusNamespaceName
-            QueueName = [string[]]$Config.serviceBusQueueName
+            QueueName = $Config.serviceBusQueueName
         }
         $Result = .\New-ServiceBus.ps1 @params
         $Result.Count | Should Be 1
     }
 
     It "Should create a Service Bus in the correct location" {
-        $Result = Get-AzureRmServiceBusNamespace  -Name $Config.serviceBusNamespaceName -ResourceGroup $Config.resourceGroupName
+        $Result = Get-AzureRmServiceBusNamespace -Name $Config.serviceBusNamespaceName -ResourceGroup $Config.resourceGroupName
         $Result.Location | Should Be $Config.location
     }
 
     It "Should create specified Service Bus queues" {
-        foreach($queue in [string[]]$Config.serviceBusQueueName){
-            $queue.Trim()
-            $ExistingQueue = Get-AzureRmServiceBusQueue -ResourceGroup $Config.resourceGroupName -NamespaceName $Config.serviceBusNamespaceName -QueueName $queue
-            $ExistingQueue.Name | Should Be $queue
+        foreach($Queue in $Config.serviceBusQueueName){
+            $Queue.Trim()
+            $ExistingQueue = Get-AzureRmServiceBusQueue -ResourceGroup $Config.resourceGroupName -NamespaceName $Config.serviceBusNamespaceName -QueueName $Queue
+            $ExistingQueue.Name | Should Be $Queue
         }
     }
 }
