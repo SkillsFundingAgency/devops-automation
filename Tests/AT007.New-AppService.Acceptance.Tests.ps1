783f91df946948f72f5f083c1d57d18755ee874e
Ewan Noble <ewan.noble@fasst.org.uk>
2017-07-20 15:54:23 +0100

    DASD-89 New-AppService acceptance test

---
+++ Tests/AT007.New-AppService.Acceptance.Tests.ps1
@@ -0,0 +1,35 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Acceptance.Config.json -Raw | ConvertFrom-Json
+Push-Location -Path $PSScriptRoot\..\Infrastructure\Resources\
+
+Describe "New-AppService Tests" -Tag "Acceptance-ARM" {
+
+    It "Should create an App service and return no inputs" {
+        $Result = .\New-AppService.ps1 -Location $Config.location -ResourceGroupName $Config.resourceGroupName -AppServicePlanName $Config.appServicePlanName `
+            -AppServiceName $Config.appServiceName
+        $Result.Count | Should Be 0
+    }
+
+    It "Should create an App service in the correct location" {
+        $Result = Get-AzureRmWebApp -ResourceGroupName $Config.resourceGroupName -Name $Config.appServiceName
+        $Result.Location | Should Be $Config.location
+    }
+
+    It "Should create an App service with the correct name" {
+        $Result = Get-AzureRmWebApp -ResourceGroupName $Config.resourceGroupName -Name $Config.appServiceName
+        $Result.Name | Should Be $Config.appServiceName
+    }
+
+    It "Should create an App service plan with the correct location" {
+        $Result = Get-AzureRmAppServicePlan -ResourceGroupName $Config.resourceGroupName -Name $Config.appServicePlanName
+        $Result.Location | Should Be $Config.location
+    }
+
+    It "Should create an App service plan with the correct name" {
+        $Result = Get-AzureRmAppServicePlan -ResourceGroupName $Config.resourceGroupName -Name $Config.appServicePlanName
+        $Result.Name | Should Be $Config.appServicePlanName
+    }
+
+
+}
+
+Pop-Location
