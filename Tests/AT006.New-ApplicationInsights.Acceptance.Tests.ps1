617a0c79f47b133adf7bdf9b469fb6d901a685a2
Ewan Noble <ewan.noble@fasst.org.uk>
2017-07-20 12:09:07 +0100

    DASD-85 New-ApplicationInsights Acceptance Test

---
+++ Tests/AT006.New-ApplicationInsights.Acceptance.Tests.ps1
@@ -0,0 +1,18 @@
+
+$Config = Get-Content $PSScriptRoot\..\Tests\Acceptance.Config.json -Raw | ConvertFrom-Json
+Push-Location -Path $PSScriptRoot\..\Infrastructure\Resources\
+
+Describe "New-ApplicationInsights Tests" -Tag "Acceptance-ARM" {
+
+    It "Should create an ApplicationInsights and return a valid instrumentation key" {
+        $Result = .\New-ApplicationInsights.ps1 -Location $Config.location -Name $Config.appInsightsName -ResourceGroupName $Config.ResourceGroupName
+        $Result.Count | Should Be 1
+        $Result.Contains("InstrumentationKey") | Should Be $true
+        $Result = $Result.Replace("##vso[task.setvariable variable=InstrumentationKey;]", "")
+        $validGuid = [System.Guid]::NewGuid()
+        $success = [System.Guid]::TryParse($Result,[ref]$validGuid);
+        $success | Should Be $true
+    }
+}
+
+Pop-Location
