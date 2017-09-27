8e4ca0b9afd52713ece35217722114dcfcce08a7
Ewan Noble <ewan.noble@fasst.org.uk>
2017-09-27 15:42:07 +0100

    KeyVault Acceptance Tests

---
+++ Tests/AT010.New-KeyVault.Acceptance.Tests.ps1
@@ -0,0 +1,34 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Acceptance.Config.json -Raw | ConvertFrom-Json
+Push-Location -Path $PSScriptRoot\..\Infrastructure\Resources\
+
+Describe "New-KeyVault Tests" -Tag "Acceptance-ARM" {
+
+    $ResourceGroupName = "$($Config.ResourceGroupName)$($Config.suffix)"
+    $KeyVaultName = "$($Config.keyVaultName)$($Config.suffix)"
+
+    It "Should create an Azure Keyvault and return an output" {
+        $KeyVaultParameters = @{
+            Location                 = $config.location
+            ResourceGroupName        = $ResourceGroupName
+            Name                     = $KeyVaultName
+            ServicePrincipalObjectId = $config.servicePrincipalObjectId
+            SecretName               = $config.keyVaultSecretName
+            SecretValue              = $config.keyVaultSecretValue
+        }
+
+        $Result = .\New-KeyVault.ps1 @KeyVaultParameters
+        $Result.Count | Should Be 1
+    }
+
+    It "Should create a KeyVault with the correct access policy" {
+        $Keyvault = Get-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName
+        ($KeyVault.AccessPolicies | Where-Object {$_.ObjectId -eq $config.servicePrincipalObjectId}).Count | Should be 1
+    }
+
+    It "Should create a KeyVault with the specified secret name and value" {
+        $Secret = Get-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $config.keyVaultSecretName
+        $Secret.SecretValueText | Should be $config.keyVaultSecretValue
+    }
+}
+
+Pop-Location
