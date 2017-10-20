a8d5c02b8929243feb28a9b842f8b8357bbca6ed
Craig Gumbley <craiggumbley@gmail.com>
2017-10-20 11:48:24 +0100

    Add tests for topics and subscriptions

--- Tests/AT008.New-ServiceBus.Acceptance.Tests.ps1
+++ Tests/AT008.New-ServiceBus.Acceptance.Tests.ps1
@@ -6,41 +6,68 @@ Describe "New-ServiceBus Tests" -Tag "Acceptance-ARM" {
     $ResourceGroupName = "$($Config.resourceGroupName)$($Config.suffix)"
     $ServiceBusName = "$($Config.serviceBusNamespaceName)$($Config.suffix)"
 
-    It "Should create a Service Bus and return one output" {
-        $Params = @{
-            Location          = $Config.location
-            ResourceGroupName = $ResourceGroupName
-            NamespaceName     = $ServiceBusName
-            QueueName         = $Config.serviceBusQueueName
-        }
-        $Result = .\New-ServiceBus.ps1 @params
-        $Result.Count | Should Be 1
-    }
+    # --- Define global properties for this test block
+    $TopicDefinitionPath = "TestDrive:\sbq.topic.definition.json"
+    $null = $Config.ServiceBusTopicDefinition | ConvertTo-Json | Set-Content -Path $TopicDefinitionPath
 
-    It "Should return one output on subsequent runs" {
+    It "Should create a Service Bus and return two outputs" {
         $Params = @{
             Location          = $Config.location
             ResourceGroupName = $ResourceGroupName
             NamespaceName     = $ServiceBusName
-            QueueName         = $Config.serviceBusQueueName
         }
         $Result = .\New-ServiceBus.ps1 @params
-        $Result.Count | Should Be 1
-    }    
+        $Result.Count | Should Be 2
+    }
 
     It "Should create a Service Bus in the correct location" {
         $Result = Get-AzureRmServiceBusNamespace -Name $ServiceBusName -ResourceGroup $ResourceGroupName
         $Result.Location | Should Be $Config.location
     }
 
-    It "Should create specified Service Bus queues" {
+    It "Should create a Queue on an existing service bus" {
+        $Params = @{
+            Location          = $Config.location
+            ResourceGroupName = $ResourceGroupName
+            NamespaceName     = $ServiceBusName
+            QueueName         = $Config.serviceBusQueueName
+        }
+        $Result = .\New-ServiceBus.ps1 @params
+
         foreach ($Queue in $Config.serviceBusQueueName) {
             $Queue.Trim()
             $ExistingQueue = Get-AzureRmServiceBusQueue -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -QueueName $Queue
             $ExistingQueue.Name | Should Be $Queue
         }
+
     }
 
+    It "Should create a Topic on an existing service bus" {
+
+        # --- Re run creation script this but this time use the Topic parameter set
+        $Params = @{
+            Location          = $Config.location
+            ResourceGroupName = $ResourceGroupName
+            NamespaceName     = $ServiceBusName
+            TopicDefinition   = $TopicDefinitionPath
+        }
+
+        $Result = .\New-ServiceBus.ps1 @params
+
+        $TopicName = $Config.ServiceBusTopicDefinition[0].TopicName
+        $Result = Get-AzureRmServiceBusTopic -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -TopicName $TopicName -ErrorAction SilentlyContinue
+        $Result.Name | Should Be $TopicName
+    }
+
+    It "Should create a subscription on an existing topic" {
+
+        $TopicName = $Config.ServiceBusTopicDefinition[0].TopicName
+        $SubscriptionName = $Config.ServiceBusTopicDefinition[0].Subscription[0]
+        $Result = Get-AzureRmServiceBusSubscription -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -TopicName $TopicName -SubscriptionName $SubscriptionName -ErrorAction SilentlyContinue
+        $Result.Name | Should Be $SubscriptionName
+    }
+
+    # --- Access Policies
     It "Should create an authorization rule named ReadWrite" {
         {Get-AzureRmServiceBusNamespaceAuthorizationRule -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -AuthorizationRuleName ReadWrite} | Should Not Throw
     }
@@ -50,6 +77,16 @@ Describe "New-ServiceBus Tests" -Tag "Acceptance-ARM" {
         $Eval = $AuthorizationRule.Rights -contains "Send" -and $AuthorizationRule.Rights -contains "Listen" 
         $Eval | Should Be $true
     }
+
+    It "Should create an authorization rule named Read" {
+        {Get-AzureRmServiceBusNamespaceAuthorizationRule -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -AuthorizationRuleName Read } | Should Not Throw
+    }
+
+    It "Should create an authorization rule with Listen rights" {
+        $AuthorizationRule = Get-AzureRmServiceBusNamespaceAuthorizationRule -ResourceGroup $ResourceGroupName -NamespaceName $ServiceBusName -AuthorizationRuleName Read
+        $Eval = $AuthorizationRule.Rights -contains "Listen" 
+        $Eval | Should Be $true
+    }    
 }
 
 Pop-Location
