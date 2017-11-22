06bfd31160c1d48fcb3da257e062c8184051d37f
Craig Gumbley <craiggumbley@gmail.com>
2017-11-22 14:55:36 +0000

    Minor code review changes

--- Infrastructure/Resources/New-ResourceGroup.ps1
+++ Infrastructure/Resources/New-ResourceGroup.ps1
@@ -31,7 +31,7 @@ Path to the Tag configuration JSON file .contents example
 
 .\New-ResourceGroup.ps1 -Name arm-rg-01 -Location "West Europe" 
 
--- Including Tags --
+.EXAMPLE
 
 .\New-ResourceGroup.ps1 -Name arm-rg-01 -Location "West Europe" -TagConfigPath "c:\temp\TagConfig.json"
 
@@ -43,21 +43,22 @@ Param (
     [String]$Location,
     [Parameter(Mandatory = $True)]
     [ValidateNotNullOrEmpty()]
-    [String]$Name,    
+    [String]$Name,
     [Parameter(Mandatory = $False)]
     [ValidateNotNullOrEmpty()]
-    [String]$TagConfigPath        
+    [String]$TagConfigPath
 )
+
 # --- Import Azure Helpers
 Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Azure.psm1).Path
 Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
 
-Write-Log -LogLevel Information -Message "Checking for existing Resource Group $Name"			 
+Write-Log -LogLevel Information -Message "Checking for existing Resource Group $Name"
 $ExistingResourceGroup = Get-AzureRmResourceGroup -Name $Name -ErrorAction SilentlyContinue
 
 # --- Enumerate Tag Value's from Config File if Parameter is set 
 If ($TagConfigPath) {
-    $TagConfig = Get-Content $TagConfigPath -Raw | ConvertFrom-Json
+    $TagConfig = Get-Content -Path $TagConfigPath -Raw | ConvertFrom-Json
     $ConfigTagValue = $TagConfig.Value | ConvertTo-Json
     $ConfigTagName = $TagConfig.Name 
     # --- Define & Set variables for Tags
@@ -66,7 +67,7 @@ If ($TagConfigPath) {
     $Tags = $ExistingResourceGroup.Tags
 }
 
-# --- Create Resource Group if it doesnt exist
+# --- Create Resource Group if it doesn't exist
 if (!$ExistingResourceGroup) {
     try {
         Write-Log -LogLevel Information -Message "Creating Resource Group"
@@ -92,28 +93,28 @@ if ($ExistingResourceGroup -and $TagConfigPath) {
             # --- See if the tag name and value match the Config values
             If ($($Tag.Name) -eq $TagConfig.Name -and $($Tag.Value) -ne $ConfigTagValue) {    
                 $TagPresentValueCorrect = $False    
-            }   
+            }
             If ($($Tag.Name) -eq $TagConfig.Name -and $($Tag.Value) -eq $ConfigTagValue) {    
                 $TagValueUpdated = $True    
-            }         
+            }
         }
         # --- Once all tags enumerated 
         # --- Overwrite value if Tag already present
         If (!$TagPresentValueCorrect) {
             $Tags[$TagConfig.Name] = $ConfigTagValue 
-            Set-AzureRmResourceGroup -Tag $Tags -Name $Name
-            Write-Log -LogLevel Information "Existing Tag Value ammended Name:$ConfigTagName Values:$ConfigTagValue "
+            $null = Set-AzureRmResourceGroup -Tag $Tags -Name $Name
+            Write-Log -LogLevel Information "Existing Tag Value amended Name:$ConfigTagName Values:$ConfigTagValue "
             $TagValueUpdated = $True
         }
         # --- Create Tag & Value if it doesn't exist
         If (!$TagValueUpdated) {
             $Tags += @{$TagConfig.Name = $ConfigTagValue}
-            Set-AzureRmResourceGroup -Tag $Tags -Name $Name
+            $null = Set-AzureRmResourceGroup -Tag $Tags -Name $Name
             Write-Log -LogLevel Information "Tag & Value Created  Name:$ConfigTagName Values:$ConfigTagValue "
         }
     }  
     catch {
-        throw "Could not ammend Tags . The Error is : $_"
+        throw "Could not amend Tags . The Error is : $_"
     }      
 }
 
