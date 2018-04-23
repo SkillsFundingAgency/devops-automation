7522c08d84c70f5d7936d08e3c911c59fc046ecf
Craig Gumbley <craiggumbley@gmail.com>
2018-04-23 15:35:10 +0100

    Support for creating containers

---
+++ Infrastructure/Resources/New-StorageAccountContainer.ps1
@@ -0,0 +1,77 @@
+<#
+
+.SYNOPSIS
+Create an ARM Storage Container.
+
+.DESCRIPTION
+Create one or more containers in a storage account
+
+.PARAMETER Location
+The location of the Resource Group. This is limited to West Europe and North Europe
+
+.PARAMETER Name
+The name of the Storage Account
+
+.PARAMETER ContainerName
+The names of one or more Containers to create in the Storage Account
+
+.EXAMPLE
+.\New-StorageAccountContainer.ps1 -Location "West Europe" -Name stracc -ContainerName public
+
+.EXAMPLE
+.\New-StorageAccountContainer.ps1 -Location "West Europe" -Name stracc -ContainerName public,private,images
+
+#>
+
+Param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("West Europe", "North Europe")]
+    [String]$Location = $ENV:Location,
+    [Parameter(Mandatory = $true)]
+    [String]$Name,
+    [Parameter(Mandatory = $true)]
+    [String[]]$ContainerName
+)
+
+try {
+    # --- Import Azure Helpers
+    Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Azure.psm1).Path
+    Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
+
+    Write-Log -LogLevel Information -Message "Resolving storage account"
+    # --- Check if storage account exists in our subscription
+    $StorageAccountResource = Find-AzureRmResource -ResourceNameEquals $Name
+    if (!$StorageAccountResource) {
+        throw "Could not find storage account $Name"
+    }
+
+    $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResource.ResourceGroupName -Name $Name
+    $StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $StorageAccountResource.ResourceGroupName -Name $Name)[0].Value
+    $StorageAccountContext = New-AzureStorageContext -StorageAccountName $Name -StorageAccountKey $StorageAccountKey
+
+    # --- Create containers in the storage account if required
+    if ($ContainerName -and $StorageAccount) {
+        foreach ($Container in $ContainerName) {
+            Write-Log -LogLevel Information -Message "Checking for existing container"
+            $ContainerExists = Get-AzureStorageContainer -Context $StorageAccountContext -Name $Container -ErrorAction SilentlyContinue
+            if (!$ContainerExists) {
+                try {
+                    Write-Log -LogLevel Information -Message "Creating container $Container"
+                    $null = New-AzureStorageContainer -Context $StorageAccountContext -Name $Container -Permission Off
+                }
+                catch {
+                    throw "Could not create container $Container : $_"
+                }
+            }
+        }
+    }
+
+    # --- If the storage account exists in this subscription get the key and set the env variable
+    if ($StorageAccount) {
+        $ConnectionString = "DefaultEndpointsProtocol=https;AccountName=$($Name);AccountKey=$($StorageAccountKey)"
+        Write-Output ("##vso[task.setvariable variable=StorageConnectionString;]$($ConnectionString)")
+    }
+}
+catch {
+    throw $_
+}
---
+++ Tests/AT012.New-StorageAccountContainer.Tests.ps1
@@ -0,0 +1,30 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Acceptance.Config.json -Raw | ConvertFrom-Json
+Push-Location -Path $PSScriptRoot\..\Infrastructure\Resources\
+Describe "New-StorageAccountContainer Tests" -Tag "Acceptance-ARM" {
+
+    $StorageAccountName = "$($Config.armStorageAccountName)$($Config.suffix)"
+    $ResourceGroupName = "$($Config.resourceGroupName)$($Config.suffix)"
+    $ContainerName = "$($Config.classicStorageContainerName)$($Config.suffix)"
+
+    try {
+        $null = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Location "West Europe" -Name $StorageAccountName -SkuName Standard_LRS
+        $StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName)[0].Value
+        $StorageAccountContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+    } catch {
+        throw $_
+    }
+
+    It "Should create a Storage Container in a Storage Account" {
+        $null = .\New-StorageAccountContainer.ps1 -Location "West Europe" -Name $StorageAccountName -ContainerName $ContainerName
+        $Container = Get-AzureStorageContainer -Context $StorageAccountContext -Name $Container -ErrorAction SilentlyContinue
+        $Container.Name | Should Be $ContainerName
+    }
+
+    It "Should complete succesfully and return one output" {
+        $Output = .\New-StorageAccountContainer.ps1 -Location "West Europe" -Name $StorageAccountName -ContainerName $ContainerName
+        $Output.Count | Should Be 1
+    }
+
+}
+
+Pop-Location
