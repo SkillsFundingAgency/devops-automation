57caf1ee300ca3f43c80bac5b1e25a8525e0ccf9
qamago <qamago@outlook.com>
2018-11-12 13:03:01 +0000

    CDN components

---
+++ Infrastructure/Resources/Set-CDN-Components.ps1
@@ -0,0 +1,45 @@
+<#
+
+.SYNOPSIS
+CDN
+
+.DESCRIPTION
+CDM
+
+.PARAMETER Environment
+The deployment environment
+
+
+.EXAMPLE
+
+
+#>
+
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]$Source,
+    [Parameter(Mandatory = $true)]
+    [String]$Destination,
+    [Parameter(Mandatory = $true)]
+    [String]$SaSToken
+)
+
+
+Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\BlobCopy.psm1).Path
+Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
+
+
+#Run BlobCopy @parameters
+
+
+$DeploymentParameters = @{
+    Source    = $Source
+    Destination   = $Destination
+    SaSToken     = $SaSToken
+
+}
+
+. "$PSScriptRoot\..\BlobCopy.psm1" @DeploymentParameters
+
+
+
